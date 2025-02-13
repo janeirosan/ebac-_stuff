a3816d4fb9b95fbdad45f4db45f61872c85c3cf5
janeirosan <rio.gdesigns@gmail.com>
2025-02-13 19:57:52 -0300

    js consertado

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
-const pokeForm = document.getElementById('poke-form');
-const trainerName = document.getElementById('name');
-const trainerAge = document.getElementById('age');
-const trainerShirt = document.getElementById('shirtnum');
-const btnSend = document.getElementById('btn-send');
-const noticeSuccess = document.getElementById('notice-scss');
-const noticeFalse = document.getElementById('notice-false');
-
-function numValidate() {
-    if (parseInt(trainerShirt.value, 10) > parseInt(trainerAge.value, 10)) {
-        return true
-    }
-    return false
-}
-trainerShirt.addEventListener('input', ()=>{
-    if (numValidate()) {
-        noticeFalse.style.display = 'none';
-        btnSend.disabled = false;
-    } else {
-        noticeFalse.style.display = 'block';
-        btnSend.disabled = true;
-    }
-})
-
-pokeForm.addEventListener('submit', function(e) {
-    let formValid = false;
-    e.preventDefault();
-    
-    formValid = numValidate(trainerAge)
-    if (formValid) {
-        noticeSuccess.style.display = 'block';
-        noticeSuccess.innerText = `Thanks, ${trainerName.value}! We'll see you at the challenge ;)`;
-    }
+$(document).ready(function(){
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const newTask = $('#task').val();
+        $(`<li>${newTask}</li>`).appendTo('ul');
+        $('#task').val('');
+    })
+    $(document).on('click','li',function(){
+        $('li').css("text-decoration", "line-through")
+    })
 })
