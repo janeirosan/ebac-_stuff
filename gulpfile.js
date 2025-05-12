0f5fb97005d4a3081c49fca990d262ac3bbdb83a
janeirosan <rio.gdesigns@gmail.com>
2025-05-12 18:13:07 -0300

    attempt at fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
+const sourceMaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
+
 
 function compressJS() {
     return gulp.src('./source/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/scripts'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
 }
 
 function compileSass() {
     return gulp.src('./source/styles/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('./maps'))
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(sourceMaps.write('./maps'))
         .pipe(gulp.dest('./build/styles'));
+    }
+    
+    async function imgSquash() {
+        const imagemin = (await import('gulp-imagemin')).default;
+        return gulp.src('./source/images/*') 
+        .pipe(imagemin()) 
+    .pipe(gulp.dest('./build/images'))
 }
 
-function imgSquash() {
-    return gulp .src("./sources/images/")
-    .pipe(imagemin())
-    .pipe(gulp.dest("./build/images"));
-    }
+function gulpDef(cb) {
+    gulp.task("imgSquash", imgSquash);
+    
+    gulp.task("watch", () => {
+        gulp.watch("./source/images/*", imgSquash)
+    });
 
-gulp.task("imgSquash", imgSquash);
-
-gulp.task("watch", () => {
-    gulp.watch("./source/image/*", imgSquash)
-});
+    cb();
+}
 
 
-exports.default = gulp;
+exports.default = gulpDef;
 exports.sass = compileSass;
 exports.watch = function() {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compileSass))
 }
 exports.compressJS = compressJS;
 
+return gulp.src('./source/images/*', { 
+    encoding: false 
+})
